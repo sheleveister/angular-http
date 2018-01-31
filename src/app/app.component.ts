5a0b1ec9edeb9956c414e3fb86d546e4236c7479
sheleveister <e.sheleveister@gmail.com>
2018-01-31 06:15:01 +0300

    create simple db

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,10 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss', '../assets/bootstrap.css']
 })
 export class AppComponent {
-  title = 'app';
+
+  public cars = [];
+
 }
